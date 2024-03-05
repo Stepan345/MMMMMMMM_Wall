cd0a3143dc7b1a661040eda0c247f48f50871410
Stepan <ssarkissow@gmail.com>
2024-03-05 15:37:14 -0500

    first commit

---
+++ src/main.py
@@ -0,0 +1,25 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       ssark                                                        #
+# 	Created:      3/5/2024, 3:25:33 PM                                         #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+import numpy as n
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+class Middle:
+    
+
+def main(){
+
+}
+
+
